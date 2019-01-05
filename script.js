cfb479d7cc8827ced247221f55611da3bc3ee10f
Marko <46076533+markof94@users.noreply.github.com>
2019-01-05 11:03:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function femiUntrigger(){
 $(document).ready(function(){
 	$("#myCarousel").carousel({interval: 4500});
 });
+
+var prefix = ["CIS-GENDERED", "PANSEXUAL", "HETERONORMATIVE", "GENDER BINARY", "MISOGYNISTIC", "PATRIARCHAL", "CREEPY", "LITERAL", "WHITE", "BINARY", "MICROAGRESSIVE", "CIS-HET", "CIS", "TRANSMISOGYNISTIC", "INTERSECTIONAL", "HOMOPHOBIC", "TRANSPHOBIC", "PEDOPHOBIC", "ZOOPHOBIC", "PROBLEMATIC", "INAPPROPRIATE", "PRIVILEGED", "TOXIC", "MASCULINE", "HARMFUL", "NARROW MINDED", "SEXIST", "BIGOTED", "RACIST", "MANSPLAINING", "MANSPREADING"];
+var suffix = ["WHITE MALE", "CIS MALE", "MALE", "BIGOT", "RACIST", "MANSPREADER", "NAZI", "HITLER"];
+
+function generateInsult(){
+	
+	var chosenPrefix = prefix[Math.floor(Math.random() * prefix.length)];
+	
+	var chosenSuffix = suffix[Math.floor(Math.random() * suffix.length)];
+	
+	document.getElementById("prefix").innerHTML = chosenPrefix;
+	document.getElementById("suffix").innerHTML = chosenSuffix;
+	
+}
+
+window.onload = generateInsult;
